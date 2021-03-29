749945f64eb0dc3dd7d7bb425e2e60821508ce03
Ankit Sah <ankitkumarsah3727@gmail.com>
2021-03-30 03:38:54 +0530

    zz

--- deployer.sh
+++ deployer.sh
@@ -4,16 +4,10 @@ set -e
 echo "Deploying application ..."
 
     # Update codebase
-    git fetch origin master
-    git reset --hard origin/master
+    git pull
 
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
-
-    # Note: If you're using queue workers, this is the place to restart them.
-
-    # Clear cache
-    php artisan optimize
 
     # Reload PHP to update opcache
     echo "" | sudo -S service php7.4-fpm reload
